46b2e148c9fab6e0e0f50c141b016d385bd3f05a
gizemyldz <49057258+gizemyldz@users.noreply.github.com>
2022-01-27 22:00:00 +0300

    Add files via upload

---
+++ www/cart-cart-module-es2015.js
@@ -0,0 +1,177 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["cart-cart-module"],{
+
+/***/ "./node_modules/raw-loader/dist/cjs.js!./src/app/cart/cart.page.html":
+/*!***************************************************************************!*\
+  !*** ./node_modules/raw-loader/dist/cjs.js!./src/app/cart/cart.page.html ***!
+  \***************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = ("<ion-header>\n  <ion-toolbar>\n    <ion-buttons slot=\"start\">\n      <ion-back-button defaultHref=\"/\"></ion-back-button>\n    </ion-buttons>\n    <ion-title>SEPETİM</ion-title>\n  </ion-toolbar>\n</ion-header>\n \n<ion-content>\n  <ion-list>\n    <ion-item *ngFor=\"let item of selectedItems\" lines=\"inset\">\n      {{ item.count }}x {{ item.name }} - {{ item.price | currency:'USD':'symbol' }}\n      <ion-label slot=\"end\" text-right>{{ (item.price * item.count) | currency:'USD':'symbol' }}</ion-label>\n    </ion-item>\n    <ion-item>\n      Total: <span slot=\"end\">{{ total | currency:'USD':'symbol' }}</span>\n    </ion-item>\n  </ion-list>\n</ion-content>\n\n<!--  \n<ion-header>\n  <ion-toolbar>\n    <ion-title>cart</ion-title>\n  </ion-toolbar>\n</ion-header>\n\n<ion-content>\n\n</ion-content>\n-->");
+
+/***/ }),
+
+/***/ "./src/app/cart/cart-routing.module.ts":
+/*!*********************************************!*\
+  !*** ./src/app/cart/cart-routing.module.ts ***!
+  \*********************************************/
+/*! exports provided: CartPageRoutingModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "CartPageRoutingModule", function() { return CartPageRoutingModule; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/__ivy_ngcc__/fesm2015/core.js");
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/router */ "./node_modules/@angular/router/__ivy_ngcc__/fesm2015/router.js");
+/* harmony import */ var _cart_page__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./cart.page */ "./src/app/cart/cart.page.ts");
+
+
+
+
+const routes = [
+    {
+        path: '',
+        component: _cart_page__WEBPACK_IMPORTED_MODULE_3__["CartPage"]
+    }
+];
+let CartPageRoutingModule = class CartPageRoutingModule {
+};
+CartPageRoutingModule = Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"])([
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+        imports: [_angular_router__WEBPACK_IMPORTED_MODULE_2__["RouterModule"].forChild(routes)],
+        exports: [_angular_router__WEBPACK_IMPORTED_MODULE_2__["RouterModule"]],
+    })
+], CartPageRoutingModule);
+
+
+
+/***/ }),
+
+/***/ "./src/app/cart/cart.module.ts":
+/*!*************************************!*\
+  !*** ./src/app/cart/cart.module.ts ***!
+  \*************************************/
+/*! exports provided: CartPageModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "CartPageModule", function() { return CartPageModule; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/__ivy_ngcc__/fesm2015/core.js");
+/* harmony import */ var _angular_common__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/common */ "./node_modules/@angular/common/__ivy_ngcc__/fesm2015/common.js");
+/* harmony import */ var _angular_forms__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/forms */ "./node_modules/@angular/forms/__ivy_ngcc__/fesm2015/forms.js");
+/* harmony import */ var _ionic_angular__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @ionic/angular */ "./node_modules/@ionic/angular/__ivy_ngcc__/fesm2015/ionic-angular.js");
+/* harmony import */ var _cart_routing_module__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./cart-routing.module */ "./src/app/cart/cart-routing.module.ts");
+/* harmony import */ var _cart_page__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./cart.page */ "./src/app/cart/cart.page.ts");
+
+
+
+
+
+
+
+let CartPageModule = class CartPageModule {
+};
+CartPageModule = Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"])([
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+        imports: [
+            _angular_common__WEBPACK_IMPORTED_MODULE_2__["CommonModule"],
+            _angular_forms__WEBPACK_IMPORTED_MODULE_3__["FormsModule"],
+            _ionic_angular__WEBPACK_IMPORTED_MODULE_4__["IonicModule"],
+            _cart_routing_module__WEBPACK_IMPORTED_MODULE_5__["CartPageRoutingModule"]
+        ],
+        declarations: [_cart_page__WEBPACK_IMPORTED_MODULE_6__["CartPage"]]
+    })
+], CartPageModule);
+
+
+
+/***/ }),
+
+/***/ "./src/app/cart/cart.page.scss":
+/*!*************************************!*\
+  !*** ./src/app/cart/cart.page.scss ***!
+  \*************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = ("\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzcmMvYXBwL2NhcnQvY2FydC5wYWdlLnNjc3MifQ== */");
+
+/***/ }),
+
+/***/ "./src/app/cart/cart.page.ts":
+/*!***********************************!*\
+  !*** ./src/app/cart/cart.page.ts ***!
+  \***********************************/
+/*! exports provided: CartPage */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "CartPage", function() { return CartPage; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _services_cart_service__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./../services/cart.service */ "./src/app/services/cart.service.ts");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/__ivy_ngcc__/fesm2015/core.js");
+
+
+
+let CartPage = class CartPage {
+    constructor(cartService) {
+        this.cartService = cartService;
+        this.selectedItems = [];
+        this.total = 0;
+    }
+    ngOnInit() {
+        let items = this.cartService.getCart();
+        let selected = {};
+        for (let obj of items) {
+            if (selected[obj.id]) {
+                selected[obj.id].count++;
+            }
+            else {
+                selected[obj.id] = Object.assign(Object.assign({}, obj), { count: 1 });
+            }
+        }
+        this.selectedItems = Object.keys(selected).map(key => selected[key]);
+        this.total = this.selectedItems.reduce((a, b) => a + (b.count * b.price), 0);
+    }
+};
+CartPage.ctorParameters = () => [
+    { type: _services_cart_service__WEBPACK_IMPORTED_MODULE_1__["CartService"] }
+];
+CartPage = Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"])([
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_2__["Component"])({
+        selector: 'app-cart',
+        template: Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__importDefault"])(__webpack_require__(/*! raw-loader!./cart.page.html */ "./node_modules/raw-loader/dist/cjs.js!./src/app/cart/cart.page.html")).default,
+        styles: [Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__importDefault"])(__webpack_require__(/*! ./cart.page.scss */ "./src/app/cart/cart.page.scss")).default]
+    })
+], CartPage);
+
+/*
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.page.html',
+  styleUrls: ['./cart.page.scss'],
+})
+export class CartPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}  */
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=cart-cart-module-es2015.js.map
---
+++ www/cart-cart-module-es5.js
@@ -0,0 +1,310 @@
+(function () {
+  function _createForOfIteratorHelper(o, allowArrayLike) { var it; if (typeof Symbol === "undefined" || o[Symbol.iterator] == null) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = o[Symbol.iterator](); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
+
+  function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+  function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+  function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+  function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+  function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+  (window["webpackJsonp"] = window["webpackJsonp"] || []).push([["cart-cart-module"], {
+    /***/
+    "./node_modules/raw-loader/dist/cjs.js!./src/app/cart/cart.page.html":
+    /*!***************************************************************************!*\
+      !*** ./node_modules/raw-loader/dist/cjs.js!./src/app/cart/cart.page.html ***!
+      \***************************************************************************/
+
+    /*! exports provided: default */
+
+    /***/
+    function node_modulesRawLoaderDistCjsJsSrcAppCartCartPageHtml(module, __webpack_exports__, __webpack_require__) {
+      "use strict";
+
+      __webpack_require__.r(__webpack_exports__);
+      /* harmony default export */
+
+
+      __webpack_exports__["default"] = "<ion-header>\n  <ion-toolbar>\n    <ion-buttons slot=\"start\">\n      <ion-back-button defaultHref=\"/\"></ion-back-button>\n    </ion-buttons>\n    <ion-title>SEPETİM</ion-title>\n  </ion-toolbar>\n</ion-header>\n \n<ion-content>\n  <ion-list>\n    <ion-item *ngFor=\"let item of selectedItems\" lines=\"inset\">\n      {{ item.count }}x {{ item.name }} - {{ item.price | currency:'USD':'symbol' }}\n      <ion-label slot=\"end\" text-right>{{ (item.price * item.count) | currency:'USD':'symbol' }}</ion-label>\n    </ion-item>\n    <ion-item>\n      Total: <span slot=\"end\">{{ total | currency:'USD':'symbol' }}</span>\n    </ion-item>\n  </ion-list>\n</ion-content>\n\n<!--  \n<ion-header>\n  <ion-toolbar>\n    <ion-title>cart</ion-title>\n  </ion-toolbar>\n</ion-header>\n\n<ion-content>\n\n</ion-content>\n-->";
+      /***/
+    },
+
+    /***/
+    "./src/app/cart/cart-routing.module.ts":
+    /*!*********************************************!*\
+      !*** ./src/app/cart/cart-routing.module.ts ***!
+      \*********************************************/
+
+    /*! exports provided: CartPageRoutingModule */
+
+    /***/
+    function srcAppCartCartRoutingModuleTs(module, __webpack_exports__, __webpack_require__) {
+      "use strict";
+
+      __webpack_require__.r(__webpack_exports__);
+      /* harmony export (binding) */
+
+
+      __webpack_require__.d(__webpack_exports__, "CartPageRoutingModule", function () {
+        return CartPageRoutingModule;
+      });
+      /* harmony import */
+
+
+      var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(
+      /*! tslib */
+      "./node_modules/tslib/tslib.es6.js");
+      /* harmony import */
+
+
+      var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(
+      /*! @angular/core */
+      "./node_modules/@angular/core/__ivy_ngcc__/fesm2015/core.js");
+      /* harmony import */
+
+
+      var _angular_router__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(
+      /*! @angular/router */
+      "./node_modules/@angular/router/__ivy_ngcc__/fesm2015/router.js");
+      /* harmony import */
+
+
+      var _cart_page__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(
+      /*! ./cart.page */
+      "./src/app/cart/cart.page.ts");
+
+      var routes = [{
+        path: '',
+        component: _cart_page__WEBPACK_IMPORTED_MODULE_3__["CartPage"]
+      }];
+
+      var CartPageRoutingModule = function CartPageRoutingModule() {
+        _classCallCheck(this, CartPageRoutingModule);
+      };
+
+      CartPageRoutingModule = Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"])([Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+        imports: [_angular_router__WEBPACK_IMPORTED_MODULE_2__["RouterModule"].forChild(routes)],
+        exports: [_angular_router__WEBPACK_IMPORTED_MODULE_2__["RouterModule"]]
+      })], CartPageRoutingModule);
+      /***/
+    },
+
+    /***/
+    "./src/app/cart/cart.module.ts":
+    /*!*************************************!*\
+      !*** ./src/app/cart/cart.module.ts ***!
+      \*************************************/
+
+    /*! exports provided: CartPageModule */
+
+    /***/
+    function srcAppCartCartModuleTs(module, __webpack_exports__, __webpack_require__) {
+      "use strict";
+
+      __webpack_require__.r(__webpack_exports__);
+      /* harmony export (binding) */
+
+
+      __webpack_require__.d(__webpack_exports__, "CartPageModule", function () {
+        return CartPageModule;
+      });
+      /* harmony import */
+
+
+      var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(
+      /*! tslib */
+      "./node_modules/tslib/tslib.es6.js");
+      /* harmony import */
+
+
+      var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(
+      /*! @angular/core */
+      "./node_modules/@angular/core/__ivy_ngcc__/fesm2015/core.js");
+      /* harmony import */
+
+
+      var _angular_common__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(
+      /*! @angular/common */
+      "./node_modules/@angular/common/__ivy_ngcc__/fesm2015/common.js");
+      /* harmony import */
+
+
+      var _angular_forms__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(
+      /*! @angular/forms */
+      "./node_modules/@angular/forms/__ivy_ngcc__/fesm2015/forms.js");
+      /* harmony import */
+
+
+      var _ionic_angular__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(
+      /*! @ionic/angular */
+      "./node_modules/@ionic/angular/__ivy_ngcc__/fesm2015/ionic-angular.js");
+      /* harmony import */
+
+
+      var _cart_routing_module__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(
+      /*! ./cart-routing.module */
+      "./src/app/cart/cart-routing.module.ts");
+      /* harmony import */
+
+
+      var _cart_page__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(
+      /*! ./cart.page */
+      "./src/app/cart/cart.page.ts");
+
+      var CartPageModule = function CartPageModule() {
+        _classCallCheck(this, CartPageModule);
+      };
+
+      CartPageModule = Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"])([Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+        imports: [_angular_common__WEBPACK_IMPORTED_MODULE_2__["CommonModule"], _angular_forms__WEBPACK_IMPORTED_MODULE_3__["FormsModule"], _ionic_angular__WEBPACK_IMPORTED_MODULE_4__["IonicModule"], _cart_routing_module__WEBPACK_IMPORTED_MODULE_5__["CartPageRoutingModule"]],
+        declarations: [_cart_page__WEBPACK_IMPORTED_MODULE_6__["CartPage"]]
+      })], CartPageModule);
+      /***/
+    },
+
+    /***/
+    "./src/app/cart/cart.page.scss":
+    /*!*************************************!*\
+      !*** ./src/app/cart/cart.page.scss ***!
+      \*************************************/
+
+    /*! exports provided: default */
+
+    /***/
+    function srcAppCartCartPageScss(module, __webpack_exports__, __webpack_require__) {
+      "use strict";
+
+      __webpack_require__.r(__webpack_exports__);
+      /* harmony default export */
+
+
+      __webpack_exports__["default"] = "\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzcmMvYXBwL2NhcnQvY2FydC5wYWdlLnNjc3MifQ== */";
+      /***/
+    },
+
+    /***/
+    "./src/app/cart/cart.page.ts":
+    /*!***********************************!*\
+      !*** ./src/app/cart/cart.page.ts ***!
+      \***********************************/
+
+    /*! exports provided: CartPage */
+
+    /***/
+    function srcAppCartCartPageTs(module, __webpack_exports__, __webpack_require__) {
+      "use strict";
+
+      __webpack_require__.r(__webpack_exports__);
+      /* harmony export (binding) */
+
+
+      __webpack_require__.d(__webpack_exports__, "CartPage", function () {
+        return CartPage;
+      });
+      /* harmony import */
+
+
+      var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(
+      /*! tslib */
+      "./node_modules/tslib/tslib.es6.js");
+      /* harmony import */
+
+
+      var _services_cart_service__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(
+      /*! ./../services/cart.service */
+      "./src/app/services/cart.service.ts");
+      /* harmony import */
+
+
+      var _angular_core__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(
+      /*! @angular/core */
+      "./node_modules/@angular/core/__ivy_ngcc__/fesm2015/core.js");
+
+      var CartPage = /*#__PURE__*/function () {
+        function CartPage(cartService) {
+          _classCallCheck(this, CartPage);
+
+          this.cartService = cartService;
+          this.selectedItems = [];
+          this.total = 0;
+        }
+
+        _createClass(CartPage, [{
+          key: "ngOnInit",
+          value: function ngOnInit() {
+            var items = this.cartService.getCart();
+            var selected = {};
+
+            var _iterator = _createForOfIteratorHelper(items),
+                _step;
+
+            try {
+              for (_iterator.s(); !(_step = _iterator.n()).done;) {
+                var obj = _step.value;
+
+                if (selected[obj.id]) {
+                  selected[obj.id].count++;
+                } else {
+                  selected[obj.id] = Object.assign(Object.assign({}, obj), {
+                    count: 1
+                  });
+                }
+              }
+            } catch (err) {
+              _iterator.e(err);
+            } finally {
+              _iterator.f();
+            }
+
+            this.selectedItems = Object.keys(selected).map(function (key) {
+              return selected[key];
+            });
+            this.total = this.selectedItems.reduce(function (a, b) {
+              return a + b.count * b.price;
+            }, 0);
+          }
+        }]);
+
+        return CartPage;
+      }();
+
+      CartPage.ctorParameters = function () {
+        return [{
+          type: _services_cart_service__WEBPACK_IMPORTED_MODULE_1__["CartService"]
+        }];
+      };
+
+      CartPage = Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"])([Object(_angular_core__WEBPACK_IMPORTED_MODULE_2__["Component"])({
+        selector: 'app-cart',
+        template: Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__importDefault"])(__webpack_require__(
+        /*! raw-loader!./cart.page.html */
+        "./node_modules/raw-loader/dist/cjs.js!./src/app/cart/cart.page.html"))["default"],
+        styles: [Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__importDefault"])(__webpack_require__(
+        /*! ./cart.page.scss */
+        "./src/app/cart/cart.page.scss"))["default"]]
+      })], CartPage);
+      /*
+      import { Component, OnInit } from '@angular/core';
+      
+      @Component({
+        selector: 'app-cart',
+        templateUrl: './cart.page.html',
+        styleUrls: ['./cart.page.scss'],
+      })
+      export class CartPage implements OnInit {
+      
+        constructor() { }
+      
+        ngOnInit() {
+        }
+      
+      }  */
+
+      /***/
+    }
+  }]);
+})();
+//# sourceMappingURL=cart-cart-module-es5.js.map
